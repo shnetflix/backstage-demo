c1781f41b09bc70f7ff3b5100b362a2d94b29568
esulzickis <esulzickis@toogoodtogo.com>
2025-02-10 20:07:20 +0100

    small changes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,6 +16,8 @@ backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
